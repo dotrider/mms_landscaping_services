7d270d2d4ca280628b05817f690703f694689273
dotrider <56666461+dotrider@users.noreply.github.com>
2020-07-15 02:50:01 -0700

    temporal api test

---
+++ src/data.js
@@ -0,0 +1,37 @@
+import React from 'react'
+ 
+const data = [
+    {
+        id: 1,
+        name: 'Maintenance',
+        img: "https://images.unsplash.com/photo-1526392587392-d1627b6c134a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80",
+        description: 'Dolore cupidatat ex labore incididunt culpa ullamco est sint commodo cillum nostrud culpa incididunt. Minim ex cillum culpa Lorem. Labore sunt exercitation velit fugiat nulla laborum labore est laborum est eiusmod et tempor. Velit magna incididunt eiusmod consequat ullamco.'
+    }, {
+        id: 2,
+        name: 'Maintenance',
+        img: "https://images.unsplash.com/photo-1532302780319-95689ab9d79a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80",
+        description: 'Dolore cupidatat ex labore incididunt culpa ullamco est sint commodo cillum nostrud culpa incididunt. Minim ex cillum culpa Lorem. Labore sunt exercitation velit fugiat nulla laborum labore est laborum est eiusmod et tempor. Velit magna incididunt eiusmod consequat ullamco.'
+    }, {
+        id: 3,
+        name: 'Maintenance',
+        img: "https://images.unsplash.com/photo-1472224371017-08207f84aaae?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80",
+        description: 'Dolore cupidatat ex labore incididunt culpa ullamco est sint commodo cillum nostrud culpa incididunt. Minim ex cillum culpa Lorem. Labore sunt exercitation velit fugiat nulla laborum labore est laborum est eiusmod et tempor. Velit magna incididunt eiusmod consequat ullamco.'
+    }, {
+        id: 4,
+        name: 'Maintenance',
+        img: "https://images.unsplash.com/photo-1561200766-6554e97d2fb2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80",
+        description: 'Dolore cupidatat ex labore incididunt culpa ullamco est sint commodo cillum nostrud culpa incididunt. Minim ex cillum culpa Lorem. Labore sunt exercitation velit fugiat nulla laborum labore est laborum est eiusmod et tempor. Velit magna incididunt eiusmod consequat ullamco.'
+    }, {
+        id: 5,
+        name: 'Tree trimming',
+        img: "https://images.unsplash.com/photo-1510921102321-af0cbcf8f3c9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1651&q=80",
+        description: 'Dolore cupidatat ex labore incididunt culpa ullamco est sint commodo cillum nostrud culpa incididunt. Minim ex cillum culpa Lorem. Labore sunt exercitation velit fugiat nulla laborum labore est laborum est eiusmod et tempor. Velit magna incididunt eiusmod consequat ullamco.'
+    }, {
+        id: 6,
+        name: 'Tree trimming',
+        img: "https://images.unsplash.com/photo-1565210608687-e4f3397edbfc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=975&q=80",
+        description: 'Dolore cupidatat ex labore incididunt culpa ullamco est sint commodo cillum nostrud culpa incididunt. Minim ex cillum culpa Lorem. Labore sunt exercitation velit fugiat nulla laborum labore est laborum est eiusmod et tempor. Velit magna incididunt eiusmod consequat ullamco.'
+    }
+]
+
+export default data
