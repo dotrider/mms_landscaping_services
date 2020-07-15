d99f44a3c9d3c922292b8dd088be0fbc67e15e9b
dotrider <56666461+dotrider@users.noreply.github.com>
2020-07-15 04:23:02 -0700

    MUI set/arranging layout

--- src/Components/Display/Display.js
+++ src/Components/Display/Display.js
@@ -1,21 +1,39 @@
 import React, { useState } from 'react'
-import {Container, Typography, CssBaseline} from '@material-ui/core';
+import {Container, Typography, CssBaseline, Grid, makeStyles, GridListTile, ListSubheader, GridList, CardMedia, CardActionArea, Card} from '@material-ui/core';
 import data from '../../data'
 import Project from '../Project/Project';
 
+const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+    },
+    paper: {
+      height: 140,
+      width: 100,
+    },
+    control: {
+      padding: theme.spacing(2),
+    },
+  }));
+
 const Display = () => {
 
+    const classes = useStyles();
     const [ projects, setProjects ] = useState(data)
     
     console.log('display', projects)
-    const mappedProjects = projects.map(project => <Project project={project}/>)
+    const mappedProjects = projects.map(project => <Project key={project.img} project={project}/>)
     return (
-        <React.Fragment>
-        <Container maxWidth="sm">
-         {mappedProjects}
-        </Container>
-      </React.Fragment>
+        <div className={classes.root}>
+            <GridList cellHeight={180} className={classes.gridList}>
+                    <GridListTile key="Subheader" cols={2} style={{ height: 'auto' }}>
+                    <ListSubheader component="div">December</ListSubheader>
+                    </GridListTile>
+                     {mappedProjects}
+            </GridList>
+    </div>
     )
 }
 
 export default Display
+
--- src/Components/Project/Project.js
+++ src/Components/Project/Project.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import { makeStyles, Paper, Grid, Typography, CardMedia } from '@material-ui/core';
+import { makeStyles, Paper, Grid, Typography, CardMedia, CardActionArea, Card, GridListTile } from '@material-ui/core';
 
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
+    maxWidth: 345
   },
   paper: {
     padding: theme.spacing(1),
@@ -20,31 +21,19 @@ const Project = ({project: {id, name, img, description}}) => {
 
     const classes = useStyles();
 
-    function FormRow() {
-      return (
-        <React.Fragment>
-          <Grid item xs={4}>
-          <CardMedia
-          component="img"
-          alt="Contemplative Reptile"
-          height="140"
-          image={img}
-          title="Contemplative Reptile"
-        />
-          </Grid>
-        </React.Fragment>
-      );
-    }
-
 
     return (
-          <div className={classes.root}>
-            <Grid container spacing={1}>
-              <Grid container item xs={12} spacing={3}>
-                <FormRow />
-              </Grid>
-            </Grid>
-          </div>
+        <Card className={classes.root}>
+            <CardActionArea>
+                <CardMedia 
+                        component="img"
+                        alt="Contemplative Reptile"
+                        height="140"
+                        image={img}
+                        title="Contemplative Reptile"
+            />
+            </CardActionArea>
+        </Card>    
         );
 }
 
