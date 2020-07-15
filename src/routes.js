b3abf8d39c116625ad819e4a9eda2b2638df68c6
dotrider <56666461+dotrider@users.noreply.github.com>
2020-07-15 00:28:47 -0700

    home added to routes

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom'
+import Home from './Components/Home/Home'
 
 export default (
 
     <Switch>
-        <Route>
-            
+        <Route exact path='/' component={Home}> 
+
         </Route>
     </Switch>
 )
