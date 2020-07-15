57bd315e95eb2891bacd8bd1e998e976acce7e79
dotrider <56666461+dotrider@users.noreply.github.com>
2020-07-15 01:46:11 -0700

    base layout set

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,10 +1,41 @@
 import React from 'react'
+import { makeStyles, AppBar, Toolbar, Typography, IconButton} from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+
+
 
 const Header = () => {
+
+    const classes = useStyles();
+
+
     return (
-        <div>
-            Header
-        </div>
+        <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6" className={classes.title}>
+              MMS Landscaping
+            </Typography>
+            <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+              <MenuIcon />
+            </IconButton>
+          </Toolbar>
+        </AppBar>
+      </div>
     )
 }
 
