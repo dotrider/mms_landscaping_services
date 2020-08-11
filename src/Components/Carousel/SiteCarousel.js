a3c7712c1a3fd554d187b1ead4b16c003eae0bf6
dotrider <56666461+dotrider@users.noreply.github.com>
2020-08-11 00:13:43 -0700

    applied mask/overlay to carousel

--- src/Components/Carousel/SiteCarousel.js
+++ src/Components/Carousel/SiteCarousel.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import Carousel from 'react-material-ui-carousel'
-import {Button, Grid, CardMedia} from '@material-ui/core'
+import {Button, Grid, CardMedia, Typography} from '@material-ui/core'
 import './SiteCarousel.css'
+import { MDBMask, MDBView} from "mdbreact";
 
 const SiteCarousel = ({data}) => {
 
@@ -11,22 +12,26 @@ const SiteCarousel = ({data}) => {
                     interval={2000}
                  >
                     {
-                        data.map(({img, description, name, id}, i) => <CardMedia 
-                            key={id}
-                            className='card-media' 
-                            component="img"
-                            alt={name}
-                            height="350"
-                            image={img}
-                            title="Contemplative Reptile"
-                        >
-                            {/* <h2>{name}</h2> */}
-                            {/* <p>{description}</p> */}
-            
-                        {/* <Button className="CheckButton">
-                            HELLLLOOO!
-                        </Button> */}
-                        </CardMedia>
+                        data.map(({img, description, name, id}, i) => <MDBView>
+                            <CardMedia 
+                                key={id}
+                                className='card-media' 
+                                component="image"
+                                alt={name}
+                                height="350"
+                                image={img}
+                                title="Contemplative Reptile"
+                            >
+                                    {/* <h2>{name}</h2> */}
+                                    {/* <p>{description}</p> */}
+                    
+                                {/* <Button className="CheckButton">
+                                    HELLLLOOO!
+                                </Button> */}
+                            </CardMedia>
+                            <MDBMask overlay='black-light'>
+                            </MDBMask>
+                        </MDBView>
                     )}
                 </Carousel>
             </Grid>
