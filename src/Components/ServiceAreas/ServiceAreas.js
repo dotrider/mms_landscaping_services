699b96c0a8c1f47b9c92f588188b01da0584bf8f
dotrider <56666461+dotrider@users.noreply.github.com>
2020-08-26 16:48:31 -0700

    service area list/columns dynamically set

--- src/Components/ServiceAreas/ServiceAreas.js
+++ src/Components/ServiceAreas/ServiceAreas.js
@@ -1,4 +1,5 @@
-import React,{ useState } from 'react'
+import React,{ useState, useEffect } from 'react'
+import locationsData from '../../servicedata'
 import CheckRoundedIcon from '@material-ui/icons/CheckRounded';
 import { 
     ListItem, 
@@ -9,22 +10,49 @@ import {
 
 
 const ServiceAreas = () => {
-        const [locations, setLocations] = useState(['Glendale', 'Peoria', 'Sun City', 'Surprise', 'El Mirage', 'Avondale', 'Litchfield', 'Goodyear', 'Tolleson', 'Buckeye', 'Scottsdale'])
+        const [locations, setLocations] = useState([])
+
+            console.log(locations, 'locations')
+
+    useEffect(() => {
+        const location = []
+        //Converting locationsData array into a two dimentional array to then map and render a two list column
+        locationsData.forEach((city, i) => {
+            if(i % 2){
+                location.push([locationsData[i -1], city])
+            }else if(i === locationsData.length -1 && i % 3 == 1){
+                location.push([city])
+            }
+        })
+        setLocations(location)
+    }, [])
+
 
     return (
         <div style={{backgroundColor: '#222326', color:'white'}}>
             <List component="nav" 
-                              >    
-                                { 
-                                      locations.map((city, i) => 
-                                        <ListItem key={i}>
+                    >    
+                    { 
+                        locations.map((city, i) => 
+                            <ListItem key={i}>
+                                <ListItemIcon>
+                                    <CheckRoundedIcon color="secondary" fontSize='large'/>
+                                </ListItemIcon>
+                                <ListItemText primary={city[0]} variant='h3'/>
+                                    {city[1]?
+                                        <>
                                             <ListItemIcon>
                                                 <CheckRoundedIcon color="secondary" fontSize='large'/>
                                             </ListItemIcon>
-                                            <ListItemText primary={city} variant='h3'/>
-                                        </ListItem>)
-                                  }
-                              </List>
+                                            <ListItemText primary={city[1]} variant='h3'/> 
+                                        </>
+                                        : 
+                                        null
+                                    }
+                            </ListItem>
+                      )
+                    }
+            </List>
         </div>
     )
 }
--- src/servicedata.js
+++ src/servicedata.js
@@ -1,9 +1,17 @@
 import React from 'react';
 
 const locations = [
-    {
-        
-    }
+    'Glendale', 
+    'Peoria', 
+    'Sun City', 
+    'Surprise', 
+    'El Mirage', 
+    'Avondale', 
+    'Litchfield', 
+    'Goodyear', 
+    'Tolleson', 
+    'Buckeye', 
+    'Scottsdale'
 ]
 
 export default locations
