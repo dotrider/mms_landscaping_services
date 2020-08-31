7908863e4a4ca765240cb1ba7f210335159b41ef
dotrider <56666461+dotrider@users.noreply.github.com>
2020-08-30 21:45:41 -0700

    fixed service route

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom'
 import Home from './Components/Home/Home'
-import Project from './Components/Project/Project'
+import Service from './Components/Service/Service'
 import Contact from './Components/Contact/Contact'
 import Display from './Components/Display/Display'
 
@@ -10,7 +10,7 @@ export default (
 
     <Switch>
         <Route exact path='/' component={Home}/> 
-        <Route path='/service/:id' component={Project}/>
+        <Route path='/service/:id' component={Service}/>
         <Route path='/contactUs' component={Contact}/> 
         <Route path='/display' component={Display}/> 
     </Switch>
