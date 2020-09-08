fe3c1889bfce79914f722f8b9bba3fdcb4c7b711
dotrider <56666461+dotrider@users.noreply.github.com>
2020-09-08 08:09:04 -0700

    aboutUs text set

--- src/Components/AboutUs/AboutUs.js
+++ src/Components/AboutUs/AboutUs.js
@@ -3,12 +3,23 @@ import {
     Grid, 
     Typography
   } from '@material-ui/core';
+import './AboutUs.css'
 
 const AboutUs = ({classes, aboutUs}) => {
+
+
+    const aboutPara = `What started as a side job in the early 2000's has expanded to MMS Landscaping services over the years and is continously been expaning to better service our community with high quality services. We strive for customer satisfaction by providing exceptional service to this day. `
+
+
+
     return (
         <Grid item xs={12} md={6} id='aboutUs'>
             <Typography variant='h2' className={classes.headings}>{aboutUs}</Typography>
-
+                <div className='about-inner-container'>
+                    <Typography  variant='h3'>
+                        {aboutPara}
+                    </Typography>
+                </div>
         </Grid>
     )
 }
--- src/Components/intro/Intro.js
+++ src/Components/intro/Intro.js
@@ -5,6 +5,10 @@ import './Intro.css'
 const Intro = () => {
 
     const companyName = 'MMS Landscaping'
+    const introPara = `Whether it's just maintance that you need or looking to transform your garden, we can help.
+    We provide full landscaping services with high quality results that will go beyong your landscaping needs.
+    With over 15 years of experience in the industry our success is dedicated to building long term relationships with our clients
+    providing high quality services.`
 
     return (
         <section className='intro-container'>
@@ -12,10 +16,7 @@ const Intro = () => {
             <Typography variant="h2">{companyName}</Typography>
             <div className='inner-intro-container'>
                 <Typography  variant='h3'>
-                    Whether it's just maintance that you need or looking to transform your garden, we can help.
-                    We provide full landscaping services with high quality results that will go beyong your landscaping needs.
-                    With over 15 years of experience in the industry our success is dedicated to building long term relationships with our clients
-                    providing high quality services.
+                    {introPara}
                 </Typography>
             </div>
         </section>
