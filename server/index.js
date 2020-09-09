106bb700b7d6417aa09bc0272480f62d2ee11822
dotrider <56666461+dotrider@users.noreply.github.com>
2020-09-09 15:52:57 -0700

    troubleshooting deployment

--- server/index.js
+++ server/index.js
@@ -1,22 +1,19 @@
 require('dotenv').config();
 const express = require('express'),
-    // { PORT_NUM } = process.env,
-    path = require('path'), // Usually moved to the start of file
+    path = require('path'), 
     cors = require('cors'),
-    app = express();
-    const { contact } = require('./controller/mailCtrl')
-    const PORT = process.env.PORT || 4545
+    app = express(),
+    { contact } = require('./controller/mailCtrl'),
+    PORT = process.env.PORT || 4545
 
 //Middleware
 app.use(cors())
 app.use(express.json())
 
 if(process.env.NODE_ENV === 'production'){
-//  app.use(express.static(__dirname + '../build'));
+    // app.use(express.static(__dirname + '../build'));
     // app.use('/static', express.static(path.join(__dirname, '../build')));
-    app.use(express.static(path.join(__dirname, '/..build')));
-
-
+    // app.use(express.static(path.join(__dirname, '/..build')));
 
     app.get('*', (req, res)=>{
     res.sendFile(path.join(__dirname, '../build/index.html'));
