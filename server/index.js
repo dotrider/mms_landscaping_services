027b9e76994f933600571dd139f994674b15a9e5
dotrider <56666461+dotrider@users.noreply.github.com>
2020-09-09 14:55:11 -0700

    updated server file

--- server/index.js
+++ server/index.js
@@ -1,9 +1,9 @@
 require('dotenv').config();
 const express = require('express'),
     // { PORT_NUM } = process.env,
+    path = require('path'), // Usually moved to the start of file
     cors = require('cors'),
     app = express();
-    
     const { contact } = require('./controller/mailCtrl')
     const PORT = process.env.PORT || 4545
 
@@ -15,7 +15,6 @@ app.use(express.json())
 if(process.env.NODE_ENV === 'production'){
  app.use(express.static(__dirname + '../build'));
 
- const path = require('path'); // Usually moved to the start of file
     app.get('*', (req, res)=>{
     res.sendFile(path.join(__dirname, '../build/index.html'));
 });
