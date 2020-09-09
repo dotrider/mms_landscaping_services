dc24c64e13f7419f9bbab9bc3023f3dc8f81cf13
dotrider <56666461+dotrider@users.noreply.github.com>
2020-09-09 16:13:33 -0700

    heroku deployment trial

--- server/index.js
+++ server/index.js
@@ -15,8 +15,14 @@ if(process.env.NODE_ENV === 'production'){
     // app.use('/static', express.static(path.join(__dirname, '../build')));
     // app.use(express.static(path.join(__dirname, '/..build')));
 
-    app.get('*', (req, res)=>{
-    res.sendFile(path.join(__dirname, '../build/index.html'));
+//     app.get('*', (req, res)=>{
+//     res.sendFile(path.join(__dirname, '../build/index.html'));
+// });
+app.use(express.static(path.join(__dirname, 'build')));
+
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 }
 
