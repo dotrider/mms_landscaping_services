d9853dcdcec29c2dd2287bc549a68e948a718ea5
dotrider <56666461+dotrider@users.noreply.github.com>
2020-09-09 16:21:34 -0700

    adjusting server files

--- server/index.js
+++ server/index.js
@@ -18,11 +18,11 @@ if(process.env.NODE_ENV === 'production'){
 //     app.get('*', (req, res)=>{
 //     res.sendFile(path.join(__dirname, '../build/index.html'));
 // });
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, '../build')));
 
 
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, '../build', 'index.html'));
 });
 }
 
